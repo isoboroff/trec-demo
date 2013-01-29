482f9613044436bbe36e8d9d0362d41e1f749d4a
Ian Soboroff <isoboroff@gmail.com>
2013-01-29 10:55:23 -0500

    Confirmed working, added tfidf, bm25, dfr, and lm similarities.

--- PromiseDemo/src/BatchSearch.java
+++ PromiseDemo/src/BatchSearch.java
@@ -3,7 +3,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Date;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
@@ -15,6 +14,7 @@ import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.similarities.*;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
 
@@ -26,17 +26,17 @@ public class BatchSearch {
 	/** Simple command-line based search demo. */
 	public static void main(String[] args) throws Exception {
 		String usage =
-				"Usage:\tjava BatchSearch [-index dir] [-field f] [-queries file]";
+				"Usage:\tjava BatchSearch [-index dir] [-simfn similarity] [-field f] [-queries file]";
 		if (args.length > 0 && ("-h".equals(args[0]) || "-help".equals(args[0]))) {
 			System.out.println(usage);
+			System.out.println("Supported similarity functions:\ndefault: DefaultSimilary (tfidf)\n");
 			System.exit(0);
 		}
 
 		String index = "index";
 		String field = "contents";
 		String queries = null;
-		int repeat = 0;
-		String queryString = null;
+		String simstring = "default";
 
 		for(int i = 0;i < args.length;i++) {
 			if ("-index".equals(args[i])) {
@@ -48,13 +48,36 @@ public class BatchSearch {
 			} else if ("-queries".equals(args[i])) {
 				queries = args[i+1];
 				i++;
+			} else if ("-simfn".equals(args[i])) {
+				simstring = args[i+1];
+				i++;
 			}
 		}
 
+		Similarity simfn = null;
+		if ("default".equals(simstring)) {
+			simfn = new DefaultSimilarity();
+		} else if ("bm25".equals(simstring)) {
+			simfn = new BM25Similarity();
+		} else if ("dfr".equals(simstring)) {
+			simfn = new DFRSimilarity(new BasicModelP(), new AfterEffectL(), new NormalizationH2());
+		} else if ("lm".equals(simstring)) {
+			simfn = new LMDirichletSimilarity();
+		}
+		if (simfn == null) {
+			System.out.println(usage);
+			System.out.println("Supported similarity functions:\ndefault: DefaultSimilary (tfidf)");
+			System.out.println("bm25: BM25Similarity (standard parameters)");
+			System.out.println("dfr: Divergence from Randomness model (PL2 variant)");
+			System.out.println("lm: Language model, Dirichlet smoothing");
+			System.exit(0);
+		}
+		
 		IndexReader reader = DirectoryReader.open(FSDirectory.open(new File(index)));
 		IndexSearcher searcher = new IndexSearcher(reader);
+		searcher.setSimilarity(simfn);
 		Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_41);
-
+		
 		BufferedReader in = null;
 		if (queries != null) {
 			in = new BufferedReader(new InputStreamReader(new FileInputStream(queries), "UTF-8"));
@@ -77,7 +100,7 @@ public class BatchSearch {
 			String[] pair = line.split(" ", 2);
 			Query query = parser.parse(pair[1]);
 
-			doBatchSearch(in, searcher, pair[0], query);
+			doBatchSearch(in, searcher, pair[0], query, simstring);
 		}
 		reader.close();
 	}
@@ -85,7 +108,7 @@ public class BatchSearch {
 	/**
 	 * This function performs a top-1000 search for the query as a basic TREC run.
 	 */
-	public static void doBatchSearch(BufferedReader in, IndexSearcher searcher, String qid, Query query)	 
+	public static void doBatchSearch(BufferedReader in, IndexSearcher searcher, String qid, Query query, String runtag)	 
 			throws IOException {
 
 		// Collect enough docs to show 5 pages
@@ -93,14 +116,13 @@ public class BatchSearch {
 		ScoreDoc[] hits = results.scoreDocs;
 
 		int numTotalHits = results.totalHits;
-		System.out.println(numTotalHits + " total matching documents");
-
+		
 		int start = 0;
 		int end = Math.min(numTotalHits, 1000);
 
 		for (int i = start; i < end; i++) {
 			Document doc = searcher.doc(hits[i].doc);
-			System.out.println(qid+" Q0 "+doc.get("docno")+" "+i+" "+hits[i].score+" runtag");
+			System.out.println(qid+" Q0 "+doc.get("docno")+" "+i+" "+hits[i].score+" "+runtag);
 		}
 	}
 }
